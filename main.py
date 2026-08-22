8d638cb590825cbb7703d736eca07fa1ca0a8fb1
Rupam Dhoundiyal <rupamdhoundiyal47@gmail.com>
2026-08-22 14:15:30 +0530

    Made a Chat gpt like token streaming Chat Bot using request lib

--- main.py
+++ main.py
@@ -1,6 +1,53 @@
-def main():
-    print("Hello from terminal-coding-agent!")
+
+# from aifc import Aifc_read
+import requests
+import json
+OLLAMA_URL = "http://localhost:11434/api/chat"
+MODEL = "qwen2.5-coder:3b"
+
+messages = []
+
+def chat(messages: list[dict]):
+    
+
+    response = requests.post(
+        OLLAMA_URL,
+        json = {
+            "model": MODEL,
+            "messages" : messages,
+            "stream" : True
+        },
+        stream = True
+    )
+    # AI_msg = response.json()["message"]["content"]
+    print("\nAI: ")
+    ai_res = ""
+    for line in response.iter_lines():
+        if line:
+            data = json.loads(line)
+            content = data["message"]["content"]
+            ai_res+=content
+            print(content, end="", flush = True)
+
+    print("\n")
+    messages.append({
+        "role" : "assistant",
+        "content" : ai_res
+    })
+    # return history
 
 
-if __name__ == "__main__":
-    main()
+while True:
+    user_input = input("You: \n")
+    messages.append(
+        {"role":"user","content": user_input }
+    )
+    if user_input.lower().strip() in ("exit", "quit", "bye"):
+        break
+    chat(messages)
+
+
+    
+    
+    
+
